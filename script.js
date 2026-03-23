0998ef57200d6057c320a6a8d867c4a1c411333c
yellowmedok <b.tytsky@gmail.com>
2026-03-23 15:11:44 +0200

    Lab 3: add ESLint config, env variables and fix repository hygiene

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// 1. Отримуємо статус ОДРАЗУ при завантаженні сторінки
+const appStatus = import.meta.env.VITE_APP_STATUS;
+const statusElement = document.getElementById('app-status');
+
+if (statusElement) {
+    statusElement.textContent = appStatus;
+}
+
+// 2. Функція розрахунку (займається тільки математикою)
 function calculate() {
     const distance = document.getElementById('distance').value;
     const consumption = document.getElementById('consumption').value;
@@ -5,8 +14,14 @@ function calculate() {
 
     if (distance > 0 && consumption > 0 && price > 0) {
         const total = (distance / 100) * consumption * price;
-        document.getElementById('result').innerText = `Вартість: ${total.toFixed(2)} грн`;
+        const resultElement = document.getElementById('result');
+        if (resultElement) {
+            resultElement.innerText = `Вартість: ${total.toFixed(2)} грн`;
+        }
     } else {
         alert("Будь ласка, введіть коректні дані");
     }
 }
+
+//явно вказуємо, що функція використовується глобально
+window.calculate = calculate;
